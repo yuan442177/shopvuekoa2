a2b1462ca10c3cdabf679db131d0858e1df201b3
yuan442177 <15973370670@163.com>
2019-07-22 15:31:54 +0800

    新增数据库操作基本架构

--- service/database/init.js
+++ service/database/init.js
@@ -1,30 +1,51 @@
 const mongoose = require('mongoose')
-const db = "mongodb://localhost:27017/simle-db"
+const db = "mongodb://localhost/shopkoadb"
+const glob = require('glob')
+const {resolve} = require('path')
 
-mongoose.Promise =  global.Promise
+exports.initSchemas = () =>{
+    glob.sync(resolve(__dirname,'./schema/','**/*.js')).forEach(require)
+}
 
 exports.connect = ()=>{
     //连接数据库
-    mongoose.connect(db, {useNewUrlParser:true},  function (err, db) {
-        callback(db)   
-    })
+    mongoose.connect(db)
 
-    //增加数据库连接的事件监听
-    mongoose.connection.on('disconnected',()=>{
-        //进行重连
-        mongoose.connect(db)
-    })
+    let maxConnectTimes = 0 
 
-    //数据库出现错误的时候
-    mongoose.connection.on('error',err=>{
-        console.log('错误')
-        console.log(err)
-        mongoose.connect(db)
-    })
+    return  new Promise((resolve,reject)=>{
+    //把所有连接放到这里
+        
+        //增加数据库监听事件
+        mongoose.connection.on('disconnected',()=>{
+            console.log('***********数据库断开***********')
+            if(maxConnectTimes<3){
+                maxConnectTimes++
+                mongoose.connect(db)    
+            }else{
+                reject()
+                throw new Error('数据库出现问题，程序无法搞定，请人为修理......')
+            }
+            
+        })
 
-    //链接打开的时候
-    mongoose.connection.once('open',()=>{
-        console.log('MongoDB Connected successfully!')
+        mongoose.connection.on('error',err=>{
+            console.log('***********数据库错误***********')
+            if(maxConnectTimes<3){
+                maxConnectTimes++
+                mongoose.connect(db)   
+            }else{
+                reject(err)
+                throw new Error('数据库出现问题，程序无法搞定，请人为修理......')
+            }
+
+        })
+        //链接打开的时
+        mongoose.connection.once('open',()=>{
+            console.log('MongoDB connected successfully') 
+            resolve()   
+        })
+
     })
 
 }
